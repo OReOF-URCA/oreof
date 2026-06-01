ba84d0a83ca730921bbbedf185f51efcafb4affa
Akcadag-Bastien <bastien.akcadag@gmail.com>
2026-06-01 10:30:22 +0200

    feat: modification de la disposition des nouveaux champs lies aux ects dans le formulaire type_diplome et ajustement du formatage du texte pour faciliter la lecture de la pull request

--- src/Form/TypeDiplomeType.php
+++ src/Form/TypeDiplomeType.php
@@ -91,6 +91,14 @@ class TypeDiplomeType extends AbstractType
             ])
             ->add('nbUeMin')
             ->add('nbUeMax')
+            ->add('hasEcts', YesNoType::class, [
+                'label' => 'Utilise les ECTS',
+                'empty_data' => true,
+            ])
+            ->add('nbEctsParSemestre', null, [
+                'label' => 'Nombre d\'ECTS par semestre (laisser vide si pas de quota fixe)',
+                'required' => false,
+            ])
             ->add('nbEctsMaxUe')
             ->add('nbEcParUe')
             ->add('ModeleMcc', ChoiceType::class, [
@@ -105,15 +113,8 @@ class TypeDiplomeType extends AbstractType
             ->add('hasProjet', YesNoType::class)
             ->add('ectsObligatoireSurEc', YesNoType::class, ['empty_data' => true])
             ->add('mcccObligatoireSurEc', YesNoType::class, ['empty_data' => true])
-            ->add('controleAssiduite', YesNoType::class, ['empty_data' => true])
-            ->add('hasEcts', YesNoType::class, [
-                'label' => 'Utilise les ECTS',
-                'empty_data' => true,
-            ])
-            ->add('nbEctsParSemestre', null, [
-                'label' => 'Nombre d\'ECTS par semestre (laisser vide si pas de quota fixe)',
-                'required' => false,
-            ]);
+            ->add('controleAssiduite', YesNoType::class, ['empty_data' => true]);
+            
     }
 
     public function configureOptions(OptionsResolver $resolver): void
