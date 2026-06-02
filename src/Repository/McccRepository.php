9e88ac43ed42e45326fb37381b8320ea1949b544
POL MARCHAL <pol.marchal@univ-reims.fr>
2026-06-02 10:49:49 +0200

    NewAnnee - Repository - Mccc

--- src/Repository/McccRepository.php
+++ src/Repository/McccRepository.php
@@ -48,7 +48,7 @@ class McccRepository extends ServiceEntityRepository
         }
     }
 
-    public function findFromAnneeUniversitaire(int $idCampagneCollecte) : array {
+    public function findFromAnneeUniversitaire(int $idCampagneCollecte, array $exclusionEtatDpe) : array {
         $qb = $this->createQueryBuilder('mccc');
 
         // Fiches matières hors diplôme
@@ -60,32 +60,62 @@ class McccRepository extends ServiceEntityRepository
             ->andWhere('mcccFirstCampagne.id = :idCampagne');
         
         // Fiches matières reliées à un parcours
-        $subQueryFicheParcours = $this->createQueryBuilder('mcccSecond')
+        $subQueryFicheParcours = $this->createQueryBuilder('mcccSecond');
+        $subQueryFicheParcours = $subQueryFicheParcours
             ->select('mcccSecond.id')
             ->join('mcccSecond.ficheMatiere', 'mcccSecondFiche')
             ->join('mcccSecondFiche.parcours', 'mcccSecondParcours')
             ->join('mcccSecondParcours.dpeParcours', 'mcccSecondDpe')
             ->join('mcccSecondDpe.campagneCollecte', 'mcccSecondCampagne')
-            ->andWhere('mcccSecondCampagne.id = :idCampagne');
+            ->andWhere('mcccSecondCampagne.id = :idCampagne')
+            ->andWhere(
+                $subQueryFicheParcours->expr()->andX(
+                    $subQueryFicheParcours->expr()->orX(
+                        $subQueryFicheParcours->expr()->isNull('mcccSecondParcours.isSoftDeleted'),
+                        $subQueryFicheParcours->expr()->eq('mcccSecondParcours.isSoftDeleted', 0)
+                    ),
+                    $subQueryFicheParcours->expr()->notIn('mcccSecondDpe.etatReconduction', ':exclusionEtatDpe')
+                )
+            );
 
         // Fiches matières mutualisées
-        $subQueryFicheMutualisee = $this->createQueryBuilder('mcccThird')
+        $subQueryFicheMutualisee = $this->createQueryBuilder('mcccThird');
+        $subQueryFicheMutualisee = $subQueryFicheMutualisee
             ->select('mcccThird.id')
             ->join('mcccThird.ficheMatiere', 'mcccThirdFiche')
             ->join('mcccThirdFiche.ficheMatiereParcours', 'mcccThirdMutu')
             ->join('mcccThirdMutu.parcours', 'mcccThirdParcours')
             ->join('mcccThirdParcours.dpeParcours', 'mcccThirdDpe')
             ->join('mcccThirdDpe.campagneCollecte', 'mcccThirdCampagne')
-            ->andWhere('mcccThirdCampagne.id = :idCampagne');
+            ->andWhere('mcccThirdCampagne.id = :idCampagne')
+            ->andWhere(
+                $subQueryFicheMutualisee->expr()->andX(
+                    $subQueryFicheMutualisee->expr()->orX(
+                        $subQueryFicheMutualisee->expr()->isNull('mcccThirdParcours.isSoftDeleted'),
+                        $subQueryFicheMutualisee->expr()->eq('mcccThirdParcours.isSoftDeleted', 0)
+                    ),
+                    $subQueryFicheMutualisee->expr()->notIn('mcccThirdDpe.etatReconduction', ':exclusionEtatDpe')
+                )
+            );
 
         // Élément constitutif reliés à un parcours
-        $subQueryElementConstitutif = $this->createQueryBuilder('mcccFourth')
+        $subQueryElementConstitutif = $this->createQueryBuilder('mcccFourth');
+        $subQueryElementConstitutif = $subQueryElementConstitutif
             ->select('mcccFourth.id')
             ->join('mcccFourth.ec', 'mcccFourthEc')
             ->join('mcccFourthEc.parcours', 'mcccFourthParcours')
             ->join('mcccFourthParcours.dpeParcours', 'mcccFourthDpe')
             ->join('mcccFourthDpe.campagneCollecte', 'mcccFourthCampagne')
-            ->andWhere('mcccFourthCampagne.id = :idCampagne');
+            ->andWhere('mcccFourthCampagne.id = :idCampagne')
+            ->andWhere(
+                $subQueryElementConstitutif->expr()->andX(
+                    $subQueryElementConstitutif->expr()->orX(
+                        $subQueryElementConstitutif->expr()->isNull('mcccFourthParcours.isSoftDeleted'),
+                        $subQueryElementConstitutif->expr()->eq('mcccFourthParcours.isSoftDeleted', 0)
+                    ),
+                    $subQueryElementConstitutif->expr()->notIn('mcccFourthDpe.etatReconduction', ':exclusionEtatDpe')
+                )
+            );
 
         return $qb 
             ->select('DISTINCT mccc.id')
@@ -98,6 +128,7 @@ class McccRepository extends ServiceEntityRepository
                 )
             )
             ->setParameter(':idCampagne', $idCampagneCollecte)
+            ->setParameter(':exclusionEtatDpe', $exclusionEtatDpe)
             ->getQuery()
             ->getResult();
     }
