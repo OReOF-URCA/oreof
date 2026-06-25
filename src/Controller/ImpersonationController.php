dfa0963086bb03445d8b5ac79647542c44e67339
davidannebicque <david.annebicque@gmail.com>
2026-06-25 22:31:58 +0200

    feat: add documentation for JsonConfigType and improve impersonation UI components

--- src/Controller/ImpersonationController.php
+++ src/Controller/ImpersonationController.php
@@ -11,6 +11,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use App\Service\DataTableBuilder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,85 +31,55 @@ class ImpersonationController extends BaseController
     }
 
     #[Route('', name: 'app_impersonation_list', methods: ['GET'])]
-    public function list(Request $request): Response
+    public function list(DataTableBuilder $builder): Response
     {
-        $search = $request->query->get('search', '');
-        $page = $request->query->getInt('page', 1);
-
-        $query = $this->userRepository->createQueryBuilder('u')
-            ->andWhere('u.isDeleted = false')
-            ->andWhere('u.isEnable = true');
-
-        if (!empty($search)) {
-            $query->andWhere(
-                $query->expr()->orX(
-                    $query->expr()->like('u.username', ':search'),
-                    $query->expr()->like('u.email', ':search'),
-                    $query->expr()->like('u.nom', ':search'),
-                    $query->expr()->like('u.prenom', ':search')
-                )
-            )
-                ->setParameter('search', '%' . $search . '%');
-        }
-
-        $query->orderBy('u.nom', 'ASC')
-            ->addOrderBy('u.prenom', 'ASC');
-
-        $total = count($query->getQuery()->getResult());
-        $users = $query
-            ->setFirstResult(($page - 1) * self::USERS_PER_PAGE)
-            ->setMaxResults(self::USERS_PER_PAGE)
-            ->getQuery()
-            ->getResult();
-
-        $totalPages = ceil($total / self::USERS_PER_PAGE);
+        $table = $builder
+            ->setEntity(User::class)
+            ->setPerPage(50)
+            ->setDefaultSort('nom')
+            ->addBaseWhere('e.isEnable = :isEnable')
+            ->addBaseWhere('e.isDeleted = :isDeleted')
+            ->addBaseParameter('isEnable', true)
+            ->addBaseParameter('isDeleted', false)
+            ->addColumn('username', [
+                'label' => 'Login',
+                'sortable' => true,
+                'filterable' => true,
+            ])
+            ->addColumn('nom', [
+                'label' => 'Nom',
+                'sortable' => true,
+                'filterable' => true,
+            ])
+            ->addColumn('prenom', [
+                'label' => 'Prénom',
+                'sortable' => true,
+                'filterable' => true,
+            ])
+            ->addColumn('email', [
+                'label' => 'Email',
+                'sortable' => true,
+                'filterable' => true,
+            ])
+            ->addColumn('roles', [
+                'label' => 'Rôles',
+                'sortable' => false,
+                'filterable' => false,
+                'template' => 'impersonation/_column_roles.html.twig',
+            ])
+            ->addColumn('id', [
+                'label' => 'Action',
+                'sortable' => false,
+                'filterable' => false,
+                'searchable' => false,
+                'template' => 'impersonation/_column_actions.html.twig',
+            ]);
 
         return $this->render('impersonation/list.html.twig', [
-            'users' => $users,
-            'search' => $search,
-            'page' => $page,
-            'totalPages' => $totalPages,
-            'total' => $total,
+            'table' => $table->build(),
         ]);
     }
 
-    #[Route('/api/search', name: 'app_impersonation_api_search', methods: ['GET'])]
-    public function apiSearch(Request $request): JsonResponse
-    {
-        $search = $request->query->get('q', '');
-
-        if (strlen($search) < 2) {
-            return $this->json([]);
-        }
-
-        $users = $this->userRepository->createQueryBuilder('u')
-            ->andWhere('u.isDeleted = false')
-            ->andWhere('u.isEnable = true')
-            ->andWhere(
-                $this->userRepository->createQueryBuilder('u')->expr()->orX(
-                    $this->userRepository->createQueryBuilder('u')->expr()->like('u.username', ':search'),
-                    $this->userRepository->createQueryBuilder('u')->expr()->like('u.email', ':search'),
-                    $this->userRepository->createQueryBuilder('u')->expr()->like('u.nom', ':search'),
-                    $this->userRepository->createQueryBuilder('u')->expr()->like('u.prenom', ':search')
-                )
-            )
-            ->setParameter('search', '%' . $search . '%')
-            ->orderBy('u.nom', 'ASC')
-            ->addOrderBy('u.prenom', 'ASC')
-            ->setMaxResults(20)
-            ->getQuery()
-            ->getResult();
-
-        return $this->json(array_map(function (User $user) {
-            return [
-                'id' => $user->getId(),
-                'username' => $user->getUsername(),
-                'display' => $user->getDisplay(),
-                'email' => $user->getEmail(),
-                'roles' => implode(', ', $user->getRoles()),
-            ];
-        }, $users));
-    }
 }
 
 
