458bb9886c342fc1c43ef42e89b25014cab46c90
POL MARCHAL <pol.marchal@univ-reims.fr>
2026-08-20 11:45:34 +0200

    repository : request to retrieve ramifications

--- src/Repository/ParcoursRamificationRepository.php
+++ src/Repository/ParcoursRamificationRepository.php
@@ -15,4 +15,40 @@ class ParcoursRamificationRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ParcoursRamification::class);
     }
+
+    public function findAllRamificationsGroups() {
+        return $this->createQueryBuilder('pr')
+            ->select([
+                'pOrigine.id AS parcours_origine_id',
+                "CONCAT(
+                    pOTd.libelle, 
+                    ' - ', 
+                    COALESCE(pOfM.libelle, pOf.mentionTexte), 
+                    ' - ', 
+                    pOrigine.libelle) 
+                        AS parcours_origine_libelle_complet",
+                'pOrigine.typeParcours AS parcours_origine_type_parcours',
+                'pCible.id AS parcours_cible_id',
+                "CONCAT(
+                    pCTd.libelle, 
+                    ' - ', 
+                    COALESCE(pCfM.libelle, pCf.mentionTexte), 
+                    ' - ', 
+                    pCible.libelle) 
+                        AS parcours_cible_libelle_complet",
+                'pCible.typeParcours AS parcours_cible_type_parcours',
+                'typeRamif.code as code_type_ramif'
+            ])
+            ->join('pr.parcoursCible', 'pCible')
+            ->join('pr.parcoursOrigine', 'pOrigine')
+            ->join('pOrigine.formation', 'pOf')
+            ->join('pOf.typeDiplome', 'pOTd')
+            ->leftJoin('pOf.mention', 'pOfM')
+            ->join('pCible.formation', 'pCf')
+            ->leftJoin('pCf.mention', 'pCfM')
+            ->join('pCf.typeDiplome', 'pCTd')
+            ->join('pr.typeRamification', 'typeRamif')
+            ->groupBy('pOrigine.id, typeRamif.id')
+            ->getQuery()->getResult();
+    }
 }
