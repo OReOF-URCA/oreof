69a1ae8134406d8b32cbfc1e097419743f35f42f
POL MARCHAL <pol.marchal@univ-reims.fr>
2026-09-04 15:27:57 +0200

    recopie nouvelle année: exclusion des 'fermetures définitives"

--- src/Command/DuplicateForNewAnneeCommand.php
+++ src/Command/DuplicateForNewAnneeCommand.php
@@ -53,9 +53,6 @@ class DuplicateForNewAnneeCommand extends Command
     private const NEW_SLUG_YEAR_SUFFIX = '-2027';
 
     private const EXCLUSION_STATE = [
-        TypeModificationDpeEnum::NON_OUVERTURE->value,
-        TypeModificationDpeEnum::NON_OUVERTURE_SES->value,
-        TypeModificationDpeEnum::NON_OUVERTURE_CFVU->value,
         TypeModificationDpeEnum::FERMETURE_DEFINITIVE->value
     ];
 
